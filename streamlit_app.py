c4bb79a090327ce5b592bbfd47bd2e59e5d5523c
zionexo <105423210+zionexo@users.noreply.github.com>
2022-05-12 13:00:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 df=pd.read_csv('dati.csv',index_col=0)
 
 
-st.title('Mobile Production Dashboard')
+st.title('Azienda 1 Dashboard')
 
 st.line_chart(data=df['Capitale a disposizione'])
 st.bar_chart(data=df['Capitale a disposizione'])
