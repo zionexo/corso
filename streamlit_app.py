9eeca24fbe53b8b6b0f1435e0ce3d8723ead06ce
zionexo <105423210+zionexo@users.noreply.github.com>
2022-05-12 18:09:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,31 @@ import altair as alt
 
 df=pd.read_csv('dati.csv',index_col=0)
 
-
 st.title('Azienda 1 Dashboard')
 
-st.bar_chart(data=df['Capitale a disposizione'])
-st.bar_chart(data=df[['Materiale disponibile da circolarità','Investimenti in processi circolari']])
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  st.bar_chart(data=df['Capitale a disposizione'])
+  st.bar_chart(data=df[['Materiale disponibile da circolarità','Investimenti in processi circolari']])
+
+with col2:
+  st.title('KPI')
+  st.bar_chart(data=df['Brand reputation.1'])
+  st.bar_chart(data=df['Importo finale / Importo iniziale'])
+  st.bar_chart(data=df['Obiettivi raggiunti'])
+
+with col3:
+  st.header("An owl")
+  st.image("https://static.streamlit.io/examples/owl.jpg")
+    
 
 
 
 
-st.title('KPI')
-st.bar_chart(data=df['Brand reputation.1'])
-st.bar_chart(data=df['Importo finale / Importo iniziale'])
-st.bar_chart(data=df['Obiettivi raggiunti'])
+
+
+
+
+
 
