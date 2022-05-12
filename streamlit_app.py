785f66cd2d44f00402cb306d2bb9cbafa1daaff2
zionexo <105423210+zionexo@users.noreply.github.com>
2022-05-12 13:04:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,33 +7,10 @@ df=pd.read_csv('dati.csv',index_col=0)
 
 st.title('Azienda 1 Dashboard')
 
-st.line_chart(data=df['Capitale a disposizione'])
 st.bar_chart(data=df['Capitale a disposizione'])
 
 
+st.bar_chart(data=df['Brand reputation.1'])
+st.bar_chart(data=df['Importo finale / Importo iniziale'])
+st.bar_chart(data=df['Obiettivi raggiunti'])
 
-
-
-
-st.text('Fixed width text')
-st.markdown('_Markdown_') # see *
-st.caption('Balloons. Hundreds of them...')
-st.latex(r''' e^{i\pi} + 1 = 0 ''')
-st.write('Most objects') # df, err, func, keras!
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-st.header('My header')
-st.subheader('My sub')
-st.code('for i in range(8): foo()')
-
-
-st.button('Hit me')
-#st.download_button('On the dl', data)
-st.checkbox('Check me out')
-st.radio('Radio', [1,2,3])
-st.selectbox('Select', [1,2,3])
-st.multiselect('Multiselect', [1,2,3])
-st.slider('Slide me', min_value=0, max_value=10)
-st.select_slider('Slide to select', options=[1,'2'])
-st.text_input('Enter some text')
-st.text_input('Enter some text')
