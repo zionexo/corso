d6ffa2f8cb1da5493b889a9419bcaa747d2da498
zionexo <105423210+zionexo@users.noreply.github.com>
2022-05-12 18:30:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,15 @@ import altair as alt
 
 df=pd.read_csv('dati.csv')
 st.dataframe(df)
-fig=alt.Chart(df).mark_line().encode(x='turno',y='Capitale a disposizione')
-st.altair_chart(fig, use_container_width=True)
+fig1=alt.Chart(df).mark_line().encode(x='turno',y='Capitale a disposizione')
+
 
 
 col1, col2 = st.columns([3,2])
 
 with col1:
   st.title('Azienda 1 Dashboard')
+  st.altair_chart(fig1, use_container_width=True)
   st.bar_chart(data=df['Capitale a disposizione'])
   st.bar_chart(data=df[['Materiale disponibile da circolarità','Investimenti in processi circolari']])
 
