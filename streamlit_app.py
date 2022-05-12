4af044926b980b2f9f5b1c1ad7bd2177fbd90a97
zionexo <105423210+zionexo@users.noreply.github.com>
2022-05-12 17:53:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 import pandas as pd
-echo "backend: TkAgg" >> ~/.matplotlib/matplotlibrc
+
 import matplotlib.pyplot as plt
 
 df=pd.read_csv('dati.csv',index_col=0)
@@ -12,19 +12,20 @@ st.title('Azienda 1 Dashboard')
 st.bar_chart(data=df['Capitale a disposizione'])
 st.bar_chart(data=df[['Materiale disponibile da circolarità','Investimenti in processi circolari']])
 
+import plotly.graph_objects as go
+import numpy as np
 
-x=df["Esternalità negative totali prodotte dall'azienda"].index
-y=df["Esternalità negative totali prodotte dall'azienda"].cumsum().values
-y2=df["Vendite"].cumsum().values
-fig, ax1 = plt.subplots()
-ax2 = ax1.twinx()
-ax2.plot(x, y2, color='k')
-ax2.set_ylabel('Vendite', color='k')
-color = ['g' if i<0 else 'r' for i in y]
-ax1.bar(x, y, color=color)
-ax1.set_ylabel('Esternalità negative', color='k')
+df["Color"] = np.where(df["Esternalità negative totali prodotte dall'azienda"].cumsum()<0, 'green', 'red')
 
-st.pyplot(fig)
+# Plot
+fig = go.Figure()
+fig.add_trace(
+    go.Bar(name='Esternalità negative',
+           y=df["Esternalità negative totali prodotte dall'azienda"].cumsum(),
+           marker_color=df['Color']))
+fig.add_trace(go.Line(name='Vendite',y=df['Vendite'],marker_color='black'))
+fig.update_layout(barmode='stack')
+st.plotly_chart(fig, use_container_width=True)
 
 
 st.title('KPI')
