a723b923d441f9bb1dceecbd1a002f1aa063b34c
zionexo <105423210+zionexo@users.noreply.github.com>
2022-05-12 17:39:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,19 @@ st.title('Azienda 1 Dashboard')
 st.bar_chart(data=df['Capitale a disposizione'])
 st.bar_chart(data=df[['Materiale disponibile da circolarità','Investimenti in processi circolari']])
 
+x=df["Esternalità negative totali prodotte dall'azienda"].index
+y=df["Esternalità negative totali prodotte dall'azienda"].cumsum().values
+y2=df["Vendite"].cumsum().values
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax2.plot(x, y2, color='k')
+ax2.set_ylabel('Vendite', color='k')
+color = ['g' if i<0 else 'r' for i in y]
+ax1.bar(x, y, color=color)
+ax1.set_ylabel('Esternalità negative', color='k')
+
+st.pyplot(fig)
+
 
 st.title('KPI')
 st.bar_chart(data=df['Brand reputation.1'])
