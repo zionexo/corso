4063f9b18031c696be3c1e5d8fc28bb69a9da805
zionexo <105423210+zionexo@users.noreply.github.com>
2022-05-12 11:42:06 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,33 @@
+
+import streamlit as st
+
+st.title('Mobile Production Dashboard')
+
+
+
+
+
+
+
+
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see *
+st.caption('Balloons. Hundreds of them...')
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
+
+
+st.button('Hit me')
+#st.download_button('On the dl', data)
+st.checkbox('Check me out')
+st.radio('Radio', [1,2,3])
+st.selectbox('Select', [1,2,3])
+st.multiselect('Multiselect', [1,2,3])
+st.slider('Slide me', min_value=0, max_value=10)
+st.select_slider('Slide to select', options=[1,'2'])
+st.text_input('Enter some text')
