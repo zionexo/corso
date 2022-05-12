feb42adb0d84f6a7621df2387e0442f0ae173862
zionexo <105423210+zionexo@users.noreply.github.com>
2022-05-12 18:07:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,20 +11,7 @@ st.title('Azienda 1 Dashboard')
 st.bar_chart(data=df['Capitale a disposizione'])
 st.bar_chart(data=df[['Materiale disponibile da circolarità','Investimenti in processi circolari']])
 
-import plotly.graph_objects as go
-import numpy as np
 
-df["Color"] = np.where(df["Esternalità negative totali prodotte dall'azienda"].cumsum()<0, 'green', 'red')
-
-# Plot
-fig = go.Figure()
-fig.add_trace(
-    go.Bar(name='Esternalità negative',
-           y=df["Esternalità negative totali prodotte dall'azienda"].cumsum(),
-           marker_color=df['Color']))
-fig.add_trace(go.Line(name='Vendite',y=df['Vendite'],marker_color='black'))
-fig.update_layout(barmode='stack')
-st.plotly_chart(fig, use_container_width=True)
 
 
 st.title('KPI')
