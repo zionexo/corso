ce4ef867a19c5d46b519cb08588d0f6bfdd148e5
zionexo <105423210+zionexo@users.noreply.github.com>
2022-06-08 17:26:48 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -13,7 +13,7 @@ st.sidebar.success("Select a page above.")
 st.markdown(
     """
     Questo sito raccoglie i dati dei vari cruscotti del business game per il
-    ## Master Strategia e Gestione della Sostenibilità Aziendale della 24Ore Business School
+    ### Master Strategia e Gestione della Sostenibilità Aziendale della 24Ore Business School
     
     (Usa la barra a sinistra per navigare nel contenuto)\n
     (se la barra non è visibile attivala cliccando su ">" in alto a sinistra)
