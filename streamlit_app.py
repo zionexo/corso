4846d3cd8c46fa3a69e053a7af9a074da8b8c66b
zionexo <105423210+zionexo@users.noreply.github.com>
2022-06-08 15:32:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,16 +18,20 @@ data=pd.read_csv('dati.csv')
 data['capitale totale']=data['Capitale a disposizione']+data['Disponibilità di magazzino']*3
 
 turno=alt.Chart(data).mark_line().encode(x='turno:O')
-capitale=turno.encode(y='Capitale a disposizione')
-magazzino=turno.encode(y='Disponibilità di magazzino')
+capitale=turno.encode(y=alt.Y('Capitale a disposizione', axis=alt.Axis(title='Capitale',titleColor='blue')))
+magazzino=turno.encode(y=alt.Y('Disponibilità di magazzino', axis=alt.Axis(title='Capitale Magazzino',titleColor='green')))
 tot=turno.encode(y='capitale totale')
 
 chart=alt.layer(
     capitale.mark_line(point=alt.OverlayMarkDef(filled=False)),
-    magazzino.mark_line(color='green',point=alt.OverlayMarkDef(color='green',filled=False)),
+    magazzino.mark_line(color='green',point=alt.OverlayMarkDef(color='green',filled=False)
+                       ),
     #tot.mark_line(strokeDash=[1,1],color='red',point=alt.OverlayMarkDef(color='red',filled=False))
+).resolve_scale(
+    y = 'independent'
 )
-chart0=chart.encode(y=alt.Y(title='Capitale')).configure(background='transparent')
+
+chart0=chart.configure(background='transparent')
 
 
 importo=turno.encode(y='Importo finale / Importo iniziale')
